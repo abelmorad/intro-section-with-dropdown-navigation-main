adf9facd3b3c28f59247a3825a26a347edbd5896
abelmorad <abelmorad97@gmail.com>
2023-05-19 15:09:00 +0800

    Update now menu can be clicked

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,12 +3,17 @@ import React from 'react';
 import logo from './../../public/images/logo.svg';
 
 export default function Header() {
+    const [menuBtnClicked, setMenuBtnClicked] = useState(false)
+    const handleMenuClick = () => {
+        setMenuBtnClicked(true);
+    }
+    
     return (
         <>
         <header>
             <img src={logo} alt="snaplogo" />
-            <button className='menu-btn'></button>
-            <nav className='active-nav-menu'>
+            <button className='menu-btn' onClick={handleMenuClick}></button>
+            <nav className='active-nav-menu' style={menuBtnClicked ? { display: 'flex' } : {}}>
             <button className='x-btn'></button>
                 <div className='nav-links'>
                     <a href="#">Features</a>
