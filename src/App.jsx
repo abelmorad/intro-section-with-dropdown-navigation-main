7c51611a2e1e5be097890e47cd3f716e0ac0ccaf
abelmorad <abelmorad97@gmail.com>
2023-05-19 20:39:27 +0800

    update

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,13 @@ import React from 'react'
 import '../scss/main.scss'
 
 import Header from './components/Header'
+import Hero from './components/Hero'
 
 export default function App() {
-
   return (
-    <main className="App">
-        <Header />
-    </main>
+    <>
+      <Header />
+      <Hero />
+    </>
   )
 }
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,10 +1,29 @@
 import React from "react";
+
 import heroImage from '../../public/images/image-hero-mobile.png';
+import databiz from '../../public/images/client-databiz.svg';
+import audiophile from '../../public/images/client-audiophile.svg';
+import maker from '../../public/images/client-maker.svg';
+import meet from '../../public/images/client-meet.svg';
 
 export default function Hero() {
     return (
         <section className="hero-page">
             <img src={heroImage} className="hero-img-mobile" alt="herobro" />
+            <div className="hero-msg-wrapper">
+                <h1>Make remote work</h1>
+                <p>
+                    Get your team in sync, no matter your location.Streamline processes, 
+                    create team rituals, and watch productivity soar.
+                </p>
+                <button className="learn-more-btn">Learn more</button>
+            </div>
+            <div className="client-wrapper">
+                <img className="client-img" src={databiz} alt="databiz" />
+                <img className="client-img" src={audiophile} alt="audiophile" />
+                <img className="client-img" src={meet} alt="maker" />
+                <img className="client-img" src={maker} alt="audiophile" />
+            </div>
         </section>
     )
 }
