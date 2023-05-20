f05005149e528f4e519c43c6da4ae8cf27eee694
abelmorad <abelmorad97@gmail.com>
2023-05-20 17:39:07 +0800

    Update company dropdown now working

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ import logo from './../../public/images/logo.svg';
 
 export default function Header() {
     //menu button and close button state
-    const [menuBtnClicked, setMenuBtnClicked] = useState(false)
+    const [ menuBtnClicked, setMenuBtnClicked] = useState(false)
     const handleMenuClick = () => {
         setMenuBtnClicked(true);
     }
@@ -13,11 +13,17 @@ export default function Header() {
     }
 
     //feature dropdown 
-    const [featureClicked, setFeatureClicked] = useState(null)
+    const [ featureClicked, setFeatureClicked] = useState(null)
     const handleFeatureClick = () => {
         setFeatureClicked(featureClicked => !featureClicked);
     }
-    
+
+    //company dropdown
+    const [ companyClicked, setCompanyClicked] = useState(null)
+    const handleCompanyClick = () => {
+        setCompanyClicked(companyClicked => !companyClicked)
+    }
+
     return (
         <header>
             <img src={logo} alt="snaplogo" />
@@ -29,8 +35,16 @@ export default function Header() {
             <button className='x-btn' onClick={handleXBtnClick}></button>
                 <div className='nav-links'>
                     <div className="feature-container">
-                        <span onClick={handleFeatureClick}  className='arrow-onlink' href="#">Features</span>
-                        <div style={featureClicked ? { display: 'flex' } : {}} className='feature-wrapper'>
+                        <span 
+                            onClick={handleFeatureClick}  
+                            className='arrow-onlink' 
+                            href="#">
+                                Features
+                        </span>
+                        <div 
+                            style={featureClicked ? { display: 'flex' } : {}} 
+                            className='feature-wrapper'
+                        >
                             <span className='icon-todo'>
                                 <a href="#">Todo List</a>
                             </span>
@@ -45,7 +59,22 @@ export default function Header() {
                             </span>
                         </div>
                     </div>
-                    <span className='arrow-onlink' href="#">Company</span>
+                    <div className="company-container">
+                        <span 
+                            onClick={handleCompanyClick} 
+                            className='arrow-onlink'
+                            href="#">
+                                Company
+                        </span>
+                        <div
+                            style={companyClicked ? { display: 'flex' } : {}}
+                            className="company-links-wrapper"
+                        >
+                            <a href="#">History</a>
+                            <a href="#">Our Team</a>
+                            <a href="#">Blog</a>
+                        </div>
+                    </div>
                     <a href="#">Careers</a>
                     <a href="#">About</a>
                 </div>
